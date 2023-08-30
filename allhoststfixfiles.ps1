57275c6c7acb8db90f2c8a2b26134d7388b45caf
OmriH-Elister <80209516+OmriH-Elister@users.noreply.github.com>
2023-08-30 12:03:55 +0300

    Add files via upload

---
+++ allhoststfixfiles.ps1
@@ -0,0 +1,9 @@
+$desthosts = @("TAB0018", "TAB0033", "TAB0078", "TAB0081")
+foreach ($rem in $desthosts){ 
+
+	$path1 = read-host "what is the SMB path of the remote damaged file?" ; `
+	$path2 = read-host "what is the SMB path of the second remote damaged file?" ; ` ; `
+	$path3 = read-host "What is the local path for the correct configuration file to be copied over the damaged ones?"
+	copy-item -path $path3 -destination $path1 ; `
+	copy-item -path $path3 -destination $path2 ; ` 
+	echo "success" ; ` }

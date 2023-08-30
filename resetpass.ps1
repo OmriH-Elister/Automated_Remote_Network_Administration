e664fd91a986e86c85819bb35a73d9abd0a72f40
OmriH-Elister <80209516+OmriH-Elister@users.noreply.github.com>
2023-08-30 12:09:28 +0300

    Update resetpass.ps1

--- resetpass.ps1
+++ resetpass.ps1
@@ -1,9 +1,7 @@
 import-module activedirectory
 $username = read-host "What is the username?"
-$domain = read-host "What ist the domain (brsh\mafila\mafilb)?"
-if ($domain -eq "mafila") { $server = "mafila.brsh.local" } elseif ($domain -eq "mafilb") { $server = "mafilb.brsh.local" } else { $server = "brsh.local" } ; `
-set-ADaccountPassword -NewPassword (convertTo-secureString -asplaintext "Aa123456" -Force) -Identity $username -server $server
-set-aduser -identity $username -changepasswordatlogon $false -server $server
+$domain = read-host "What is the domain?" `
+set-ADaccountPassword -NewPassword (convertTo-secureString -asplaintext "Aa123456" -Force) -Identity $username -server $domain
 unlock-adaccount -identity $username -server $server
 write-host "All done. Tell the user to try and use the password 'Aa123456'"
 pause

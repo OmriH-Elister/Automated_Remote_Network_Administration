da2da0bef72c15d618703f4f34c2c720d1632e86
OmriH-Elister <80209516+OmriH-Elister@users.noreply.github.com>
2023-11-19 14:48:24 +0200

    Add files via upload

---
+++ Alltabs_RoutineMaintenance.ps1
@@ -0,0 +1,9 @@
+$desthosts = Get-Content -Path $home\desktop\all_hostnames.txt
+foreach ($rem in $desthosts){ 
+	invoke-command -computername  $rem -scriptblock {
+			cmd.exe /c del /q /f /s %systemroot%\temp\* ; `
+			cmd.exe /c del /q /f /s %TEMP%\* ; `
+			cmd.exe /c defrag c: ; `
+			cmd /c: sfc /scannow 
+			}
+	}

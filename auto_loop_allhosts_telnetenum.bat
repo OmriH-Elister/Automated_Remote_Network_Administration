fa741b1ad793308aa58bf15f68328768c4dddc2c
OmriH-Elister <80209516+OmriH-Elister@users.noreply.github.com>
2024-01-11 09:32:04 +0200

    Update auto_loop_allhosts_telnetenum.bat

--- auto_loop_allhosts_telnetenum.bat
+++ auto_loop_allhosts_telnetenum.bat
@@ -1,4 +1,4 @@
 @echo off 
 %password% = copy /p "please insert password"
-for /l %%I in (1,1,255) do telnet -l $domain\$username 150.130.112.%%I -f >> allhostsinfo.txt 
-for /l %%I in (1,1,255) do systeminfo /U $domain\$username /p /s 10.63.199.%%i >>allhostsinfo2.txt
+for /l %%I in (1,1,255) do telnet -l %userdomain%\%username% 150.130.112.%%I -f >> allhostsinfo.txt 
+for /l %%I in (1,1,255) do systeminfo /U %userdomain%\%username% /p /s 10.63.199.%%i >> allhostsinfo2.txt

7581054e34aa8c54088b6fd4ad9ee2a67536936e
OmriH-Elister <80209516+OmriH-Elister@users.noreply.github.com>
2024-04-10 20:38:06 +0300

    Update allhostststartwinrm.ps1

--- allhostststartwinrm.ps1
+++ allhostststartwinrm.ps1
@@ -1,3 +1,3 @@
-$desthosts = Get-Content -Path $home\desktop\hosts_tablet.txt
+$desthosts = Get-Content -Path $home\desktop\hosts.txt
 foreach ($rem in $desthosts){ 
 	Invoke-command -command { cmd.exe /c sc \\$rem start WinRM } }

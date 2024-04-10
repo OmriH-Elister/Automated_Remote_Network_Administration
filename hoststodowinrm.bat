ee04da550c49ad99f792a818d0ad374e69554502
OmriH-Elister <80209516+OmriH-Elister@users.noreply.github.com>
2024-04-10 20:44:09 +0300

    Update hoststodowinrm.bat

--- hoststodowinrm.bat
+++ hoststodowinrm.bat
@@ -1,3 +1,3 @@
 @echo off
-set HOSTS_FILE=hosts_tablet.txt
-for /f "delims=\n tokens=*" %%A in ('type hosts_tablet.txt') do (sc %A% start WinRM)
+set HOSTS_FILE=hosts.txt
+for /f "delims=\n tokens=*" %%A in ('type hosts.txt') do (sc %A% start WinRM)

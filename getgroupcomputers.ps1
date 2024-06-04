abc898fcf03f4c2f9f0d24cac6777c532434a538
Omri Herman <80209516+OmriH-Elister@users.noreply.github.com>
2024-06-04 07:43:26 +0300

    Update and rename getgroupcomputers.ps1 to corroborate_domains_computers.ps1

--- getgroupcomputers.ps1
+++
@@ -1,4 +0,0 @@
-$domain = read-host "what is the domain name?"
-$desthosts = get-content $myhosts
-foreach($dest in $desthosts){
-	get-adgroupmember -identity "Domain Computers" -Server "$domain.local" | where-object -Property distinguishedName -Like "*OU=$dest*" | select-object -property name  | tee-object .\outputhosts.txt -append }

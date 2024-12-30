7c4beed019beeaad469b7146710f3c17aeb310cd
3listeRutbot <omh_il@yahoo.com>
2024-12-30 15:01:36 +0200

    Changed hardcoded chosen password into a user-input supplied one

--- resetpass.ps1
+++ resetpass.ps1
@@ -1,7 +1,8 @@
 import-module activedirectory
 $username = read-host "What is the username?"
-$domain = read-host "What is the domain?" `
-set-ADaccountPassword -NewPassword (convertTo-secureString -asplaintext "passwd" -Force) -Identity $username -server $domain
+$domain = read-host "What is the domain?"
+$pwd = read-host "What is the chosen password?"
+set-ADaccountPassword -NewPassword (convertTo-secureString -asplaintext $pwd -Force) -Identity $username -server $domain
 unlock-adaccount -identity $username -server $server
 write-host "All done. Tell the user to try and use the password 'passwd'"
 pause

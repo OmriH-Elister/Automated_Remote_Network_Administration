5c2bbb3d03f81e911d9b4a9d2227d46db55bda85
3listeRutbot <omh_il@yahoo.com>
2024-12-31 14:13:24 +0200

    Tweaked script to pause before attempting any reboots

--- restart_computer.ps1
+++ restart_computer.ps1
@@ -1,4 +1,5 @@
 $HNA = read-host "This script automates the process of rebooting several remote LAN hosts in bulk. It needs a 'Remote-hosts.txt' file to be placed in its present working directory or else it will NOT be able to reboot more than one computer. In such a case it will ask you to provide a single hostname of a remote host and proceed to reboot it."
+pause
 if $(test-path Remote-hosts.txt){
 	foreach $rem in $(get-content Remote-hosts.txt){
 	restart-computer -computername $hostname -credential $env:USERDOMAIN\$env:USERNAME

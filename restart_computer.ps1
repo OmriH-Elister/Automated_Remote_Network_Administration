48602cd187a30ed1c927f28c2c1c4db1d2e3f338
3listeRutbot <omh_il@yahoo.com>
2024-12-31 15:15:47 +0200

    Tweaked script to pause before attempting any reboots

--- restart_computer.ps1
+++ restart_computer.ps1
@@ -1,9 +1,25 @@
-$HNA = read-host "This script automates the process of rebooting several remote LAN hosts in bulk. It needs a 'Remote-hosts.txt' file to be placed in its present working directory or else it will NOT be able to reboot more than one computer. In such a case it will ask you to provide a single hostname of a remote host and proceed to reboot it."
-pause
-if $(test-path Remote-hosts.txt){
-	foreach $rem in $(get-content Remote-hosts.txt){
-	restart-computer -computername $hostname -credential $env:USERDOMAIN\$env:USERNAME
+while ($True)
+	{
+	$choice1 = read-host "This script automates the process of rebooting several remote LAN hosts in bulk. `n`r It needs a 'Remote-hosts.txt' file to be placed in its present working directory or else it will NOT be able to reboot more than one computer. In such a case it will ask you to provide a single hostname of a remote host and proceed to reboot it. `r`n Do you wish to proceed?(y/n)"	
+	if ($choice1 = 'y'){
+		if ($(test-path Remote-hosts.txt) -eq $True){
+			foreach ($rem in $(get-content Remote-hosts.txt)){
+				restart-computer -computername $hostname -credential $env:USERDOMAIN\$env:USERNAME
+				}
+				clear
+				write-host "All done!"
+				pause
+				exit
+				}	
+		else {
+			$HNA = read-host "No 'Remote-hosts.txt' file found => no bulk restart action initiated. What is the workstation you wish to reboot's hostname?"
+		}
 	}
+	elseif ( $choice1 -eq 'n' ){
+		write-host "Bye" ; 
+		exit
+		}
+	else{
+		clear
+		write-host "Invalid choice!"}
 	}
-else {
-	$HNA read-host "No 'Remote-hosts.txt' file found => no bulk restart action initiated. What is the workstation you wish to reboot's hostname?"

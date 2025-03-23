e1ba66831bdddead065e480a5145b553865b6bcf
3listeRutbot <omh_il@yahoo.com>
2025-03-23 10:09:12 +0200

    Changed the shadow admin removal script's name

--- remove_shadoadmin_4list.ps1
+++
@@ -1,9 +0,0 @@
-$hosts = get-content allhosts.txt
-foreach ($int in $hosts){ invoke-command -computername $int -scriptblock { 
-	set-executionpolicy bypass ; 
-	remove-localgroupmember -group administrators -member sombregato ;
-	remove-localuser -name sombregato ;
-	cmd /c 'echo yes | reg delete "HKLM\SYSTEM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" ';
-	echo 'success'
-	}
-	}

d8e64167811749f48a86365845ddfdf0fcc42bdb
Omri Herman <80209516+OmriH-Elister@users.noreply.github.com>
2025-06-08 09:41:36 +0300

    Add files via upload

---
+++ find_user.ps1
@@ -0,0 +1,17 @@
+﻿$userToFind = Read-Host "Which user do you wish to locate?"
+$Domain = Read-Host "On which Domain? (FQDN format)"
+$Computers = Get-ADComputer -Filter {name -like "[insert hostname format here]*"} -server $Domain -ErrorAction SilentlyContinue | Select-object -ExpandProperty Name
+if ($Computers -ne $null ){
+    Invoke-command -ComputerName $Computers -ScriptBlock {
+        query user | foreach-object {
+            if ($_ -match "$using:userToFind") {
+                write-output "$env:COMPUTERNAME : $_" 
+                break
+                exit
+                } 
+            } 
+            } -ErrorAction SilentlyContinue 
+            }
+pause
+       
+     

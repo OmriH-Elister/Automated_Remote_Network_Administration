0bf202005a05577d58bf7c0e154eb5fa1a8643a4
Omri Herman <omh_il@yahoo.com>
2025-10-23 10:00:00 +0300

    Fix unlock account command to use domain

--- resetpass.ps1
+++ resetpass.ps1
@@ -3,6 +3,6 @@ $username = read-host "What is the username?"
 $domain = read-host "What is the domain?"
 $pwd = read-host "What is the chosen password?"
 set-ADaccountPassword -NewPassword (convertTo-secureString -asplaintext $pwd -Force) -Identity $username -server $domain
-unlock-adaccount -identity $username -server $server
-write-host "All done. Tell the user to try and use the password 'passwd'"
+unlock-adaccount -identity $username -server $domain
+write-host "All done. Tell the user to try and use the password '$pwd'"
 pause
